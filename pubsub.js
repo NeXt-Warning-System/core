364a9006ea18be572edd6141a820e5c5dee2603e
David Stone <davidjamesstone@gmail.com>
2021-01-28 12:33:52 +0000

    Publish/subscribe interface

--- pubsub.js
+++ pubsub.js
@@ -1,19 +1,35 @@
-const client = require('amqplib')
+const amqplib = require('amqplib')
 
-async function pubsub (amqpUri, topic) {
-  const connection = await client.connect(amqpUri)
+async function pubsub (url, topic) {
+  const connection = await amqplib.connect(url)
   const channel = await connection.createChannel()
   await channel.assertExchange(topic, 'fanout', { durable: false })
 
   return channel
 }
 
-async function publish (amqpUri, topic, payload) {
-  const channel = pubsub(amqpUri, topic)
-
+async function publish (url, topic, payload) {
+  const channel = await pubsub(url, topic)
   const publishResult = channel.publish(topic, '', Buffer.from(JSON.stringify(payload)))
 
   return publishResult
 }
 
-module.exports = { client, publish }
+async function subscribe (url, topic, consumer) {
+  const channel = await pubsub(url, topic)
+  const assertQueue = await channel.assertQueue('', { exclusive: true })
+  await channel.bindQueue(assertQueue.queue, topic, '')
+
+  return channel.consume(assertQueue.queue, msg => {
+    const json = msg.content.toString()
+    const payload = JSON.parse(json)
+    consumer(payload)
+  }, {
+    noAck: true
+  })
+}
+
+module.exports = {
+  publish,
+  subscribe
+}
